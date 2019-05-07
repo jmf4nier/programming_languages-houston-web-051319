b2eed4ad5da8333e1b4cb8d57a3e1513f74647e0
Jason Fournier <jmf4nier@hotmail.com>
2019-05-07 18:51:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
 
    languages.each do |style, description|
     description.each do |lang, type|
-      if new_hash.has_key?(lang)
+      if new_hash.inlcude?(lang)
         new_hash[lang][:style] << style
       else
         new_hash[lang] = type
